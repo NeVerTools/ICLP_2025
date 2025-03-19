51d2dbc5c0c2f711e4357b9b3734a01b7dc1dcf6
aims-lab <stefano.demarchi@edu.unige.it>
2025-03-19 15:42:20 +0100

    Add sample index to benchmarks

--- Generators/generate_benchmark.py
+++ Generators/generate_benchmark.py
@@ -6,20 +6,21 @@ from pynever.nodes import FullyConnectedNode, ReLUNode
 from pynever.strategies.conversion.converters.onnx import ONNXConverter
 
 
-def load_weights(m: int, p: int) -> list:
+def load_weights(i: int, m: int, p: int) -> list:
     """
     Load the weights from files.
-    The files are of the form w_<model>_<precision>.csv for linear models
-    and fc1/2_<model>_<precision>.csv for deep models.
+    The files are of the form w_<index>_<model>_<precision>.csv for linear models
+    and fc1/2_<index>_<model>_<precision>.csv for deep models.
 
     """
 
     if m < 2:
-        with open(f'Data/weights/w_{m}_{p}.csv', 'r') as w_f:
+        with open(f'Data/weights/w_{i}_{m}_{p}.csv', 'r') as w_f:
             lines = w_f.readlines()
             return [np.array([l.strip('\n').split(',') for l in lines], dtype=float)]
     else:
-        with open(f'Data/weights/fc1_{m}_{p}.csv', 'r') as fc1, open(f'Data/weights/fc2_{m}_{p}.csv', 'r') as fc2:
+        with (open(f'Data/weights/fc1_{i}_{m}_{p}.csv', 'r') as fc1,
+              open(f'Data/weights/fc2_{i}_{m}_{p}.csv', 'r') as fc2):
             lines1 = fc1.readlines()
             lines2 = fc2.readlines()
             weights1 = np.array([l.strip('\n').split(',') for l in lines1], dtype=float)
@@ -27,10 +28,11 @@ def load_weights(m: int, p: int) -> list:
             return [weights1, weights2]
 
 
-def main(model: int, precision: int):
+def main(index: int, model: int, precision: int):
     """
     Script to recreate the benchmarks in the Experiments package
 
+    :param index: the example to consider
     :param model: the model to load (0: binary linear, 1: multiclass linear, 2: binary deep, 3: multiclass deep)
     :param precision: the precision of the model (only 2 and 4 available)
     """
@@ -38,12 +40,12 @@ def main(model: int, precision: int):
     assert model in [0, 1, 2, 3] and precision in [2, 4], "Incorrect arguments, please read documentation"
 
     # Open weights
-    weights = load_weights(model, precision)
+    weights = load_weights(index, model, precision)
 
     # Create network
     ntype = 'bin' if model % 2 == 0 else 'multi'
     arch = 'lin' if model < 2 else 'deep'
-    net_name = f'mnist_{ntype}_{arch}_{precision}'
+    net_name = f'mnist_{index}_{ntype}_{arch}_{precision}'
 
     nn = SequentialNetwork(net_name, 'X')
 
@@ -59,16 +61,16 @@ def main(model: int, precision: int):
     ONNXConverter().from_neural_network(nn).save(f'../Experiments/Networks/{net_name}.onnx')
 
     # Open corresponding xv
-    with open(f'Data/points/xv_{model}_{precision}.csv', 'r') as xv_f:
+    with open(f'Data/points/xv_{index}_{model}_{precision}.csv', 'r') as xv_f:
         lines = xv_f.readlines()
         xv = np.array([l.strip('\n').split(',') for l in lines], dtype=float)
 
     # Create property
     eps = 10 ** -precision
-    with open(f'Data/labels/yt_{model}_{precision}.csv', 'r') as lab:
+    with open(f'Data/labels/yt_{index}_{model}_{precision}.csv', 'r') as lab:
         l = int(lab.read())
 
-    with open(f'../Experiments/Properties/prop_{ntype}_{arch}_{precision}_label_{l}.vnnlib', 'w') as p:
+    with open(f'../Experiments/Properties/prop_{index}_{ntype}_{arch}_{precision}_label_{l}.vnnlib', 'w') as p:
         # Variables
         for i in range(xv.shape[0]):
             p.write(f'(declare-const X_{i} Real)\n')
@@ -98,7 +100,7 @@ def main(model: int, precision: int):
 
 if __name__ == '__main__':
     try:
-        main(int(sys.argv[1]), int(sys.argv[2]))
+        main(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
     except Exception as e:
         print(e)
-        print('Usage: python generate_benchmark.py <model number> <precision> (read comments)')
+        print('Usage: python generate_benchmark.py <sample index> <model number> <precision> (read comments)')
