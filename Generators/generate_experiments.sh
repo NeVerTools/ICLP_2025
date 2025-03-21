f6ead844b4921fb9d7b9b58d2844f4dd0892ce9c
aims-lab <stefano.demarchi@edu.unige.it>
2025-03-21 10:51:37 +0100

    Update generation script

--- Generators/generate_experiments.sh
+++ Generators/generate_experiments.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-Execute MATLAB for the different settings
-python generate_benchmark.py for all settings
+# Run all MATLAB scripts to create the benchmarks
+matlab -batch "MATLAB/BinaryLinear/main0; MATLAB/BinaryLinear/main1"
+matlab -batch "MATLAB/MultiLinear/main0; MATLAB/MultiLinear/main1"
+matlab -batch "MATLAB/BinaryDeep/main0; MATLAB/BinaryDeep/main1"
+matlab -batch "MATLAB/MultiDeep/main0; MATLAB/MultiDeep/main1"
+
+# All files created, generate the networks and properties
+# The parameters are fixed in the MATLAB scripts, one can change
+# the sample index and the precision inside
+python generate_benchmark.py 0 0 3
+python generate_benchmark.py 0 1 3
+python generate_benchmark.py 1 2 3
+python generate_benchmark.py 5 3 3
