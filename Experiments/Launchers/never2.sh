a00a762e67a595a8b1d2757e28fd4af1513d8ee4
Stefano Demarchi <stefano_demarchi@outlook.com>
2025-06-05 15:40:45 +0200

    Update never2.sh

--- Experiments/Launchers/never2.sh
+++ Experiments/Launchers/never2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conda activate iclp_base
+conda activate ifm_base
 python ../../../pyNever/never2_batch.py -o output_0.csv ../instances_0.csv ./ ssbp
 python ../../../pyNever/never2_batch.py -o output_1.csv ../instances_1.csv ./ ssbp
 python ../../../pyNever/never2_batch.py -o output_2.csv ../instances_2.csv ./ ssbp
